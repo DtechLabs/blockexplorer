d8f6d726779d9440d9d665c3151cc523a53af3fe
Yuri Drigin <yuri.drigin@icloud.com>
2024-04-09 18:16:30 +0400

    Finished project

--- src/App.js
+++ src/App.js
@@ -21,16 +21,72 @@ const alchemy = new Alchemy(settings);
 
 function App() {
   const [blockNumber, setBlockNumber] = useState();
+  const [blockDetails, setBlockDetails] = useState({});
+  const [transactions, setTransactions] = useState([]);
 
   useEffect(() => {
-    async function getBlockNumber() {
-      setBlockNumber(await alchemy.core.getBlockNumber());
-    }
+    const fetchBlockDetails = async () => {
+      if (blockNumber) {
+        const block = await alchemy.core.getBlock(blockNumber);
+        setBlockDetails({
+          hash: block.hash,
+          parentHash: block.parentHash,
+          timestamp: block.timestamp,
+          nonce: block.nonce,
+          difficulty: block.difficulty,
+          gasLimit: block.gasLimit.toString(),
+          gasUsed: block.gasUsed.toString(),
+          miner: block.miner,
+          extraData: block.extraData,
+          baseFeePerGas: block.baseFeePerGas?.toString() || 'N/A' // Handle case where baseFeePerGas might not be present
+        });
 
-    getBlockNumber();
+        setTransactions(block.transactions);
+      }
+    };
+
+    fetchBlockDetails();
+  }, [blockNumber]); // This effect runs when blockNumber changes
+
+  alchemy.ws.on("block", (blockNumber) => { 
+    setBlockNumber(blockNumber);
+    console.log("New block", blockNumber);
   });
 
-  return <div className="App">Block Number: {blockNumber}</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <label htmlFor="info">Last block:</label>
+        <span id="info">{blockNumber}</span>
+      </div>
+      <div className="container table-responsive">
+        <table className="table">
+          <tbody>
+            {Object.entries(blockDetails).map(([key, value]) => (
+              <tr key={key}>
+                <td className="detail-label">{key.charAt(0).toUpperCase() + key.slice(1).replace(/([A-Z])/g, ' $1').trim()}:</td>
+                <td className="detail-value">{value}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="container table-container">
+        <h5>Transactions</h5>
+        <div className="scrollable-table">
+          <table className="table table-striped">
+            <tbody>
+              {transactions.map((tx, index) => (
+                <tr key={index}>
+                  <td>{tx}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
